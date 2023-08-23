a1256bf91d9bcc6ad698f8520ad708d2b987d219
V <pradeep.c.v@capgemini.com>
2023-08-23 12:03:39 +0530

    first commit

---
+++ src/test/java/net/guides/springboot2/springboot2jpacrudexample/ApplicationTests.java
@@ -0,0 +1,15 @@
+package net.guides.springboot2.springboot2jpacrudexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ src/test/java/net/guides/springboot2/springboot2jpacrudexample/RunWith.java
@@ -0,0 +1,9 @@
+package net.guides.springboot2.springboot2jpacrudexample;
+
+import org.springframework.test.context.junit4.SpringRunner;
+
+public @interface RunWith {
+
+    Class<SpringRunner> value();
+
+}
