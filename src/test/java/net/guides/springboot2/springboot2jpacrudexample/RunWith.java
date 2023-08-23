b1c9880b3c6390ac5b74762b4d1db4b0bd2d0595
vprad <100353910+vprad@users.noreply.github.com>
2023-08-23 13:18:26 +0530

    Delete RunWith.java

--- src/test/java/net/guides/springboot2/springboot2jpacrudexample/RunWith.java
+++
@@ -1,9 +0,0 @@
-package net.guides.springboot2.springboot2jpacrudexample;
-
-import org.springframework.test.context.junit4.SpringRunner;
-
-public @interface RunWith {
-
-    Class<SpringRunner> value();
-
-}
