aef932e563ee898b8f762e8ba7d4837d38ce210e
vprad <100353910+vprad@users.noreply.github.com>
2023-08-23 13:11:24 +0530

    Update ApplicationTests.java

--- src/test/java/net/guides/springboot2/springboot2jpacrudexample/ApplicationTests.java
+++ src/test/java/net/guides/springboot2/springboot2jpacrudexample/ApplicationTests.java
@@ -3,6 +3,8 @@ package net.guides.springboot2.springboot2jpacrudexample;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import javax.persistence.EntityManager;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
